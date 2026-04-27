e50d7a42d3c3c7a7661ee3b366fe7b9341af03f5
YouRan4 <68091576+YouRan4@users.noreply.github.com>
2026-04-27 12:13:40 +0800

    Update dns

--- merge.js
+++ merge.js
@@ -18,30 +18,30 @@ const regions = [
 
 function main(config) {
     // --- DNS 设置 ---
-    config.dns = {
-        enable: true,
-        ipv6: true,
-        "prefer-h3": true,
-        "default-nameserver": [
-            "tls://223.5.5.5:853",
-            "tls://223.6.6.6:853",
-            "tls://[2400:3200::1]:853",
-            "tls://[2402:4e00::]:853"
-        ],
-        nameserver: [
-            "https://cloudflare-dns.com/dns-query",
-            "https://dns.google/dns-query"
-        ],
-        "proxy-server-nameserver": [
-            "https://dns.alidns.com/dns-query",
-            "https://doh.pub/dns-query"
-        ],
-        "direct-nameserver": [
-            "https://dns.alidns.com/dns-query",
-            "https://doh.pub/dns-query"
-        ],
-        "respect-rules": true
-    };
+    config["dns"] = {
+    "enable": true,
+    "ipv6": true,
+    "prefer-h3": true,
+    "default-nameserver": [
+      "tls://223.5.5.5:853",
+      "tls://223.6.6.6:853",
+      "tls://[2400:3200::1]:853",
+      "tls://[2402:4e00::]:853"
+    ],
+    "nameserver": [
+      "https://cloudflare-dns.com/dns-query",
+      "https://dns.google/dns-query"
+    ],
+    "proxy-server-nameserver": [
+      "https://dns.alidns.com/dns-query",
+      "https://doh.pub/dns-query"
+    ],
+    "direct-nameserver": [
+      "https://dns.alidns.com/dns-query",
+      "https://doh.pub/dns-query"
+    ],
+    "respect-rules": true
+  };
 
     // --- 代理组生成 ---
     let proxyGroups = [
